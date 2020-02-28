84c35df0e13fedb45be0d2bc790959d04a3d22c3
Adolfo Lopez <alopezcia@gmail.com>
2020-02-28 14:34:12 +0100

    Initital

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Store, Action } from '@ngrx/store';
+import { IncrementarAction, DecrementarAction } from './contador/contador.actions';
+
+
+interface AppState {
+  contador: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redux-app';
+  contador: number;
+
+  constructor( private store: Store<AppState> ){
+    // this.contador = 10;
+    this.store.subscribe( state => {
+      this.contador = state.contador;
+//      console.log(state);
+    });
+  }
+
+  incrementar(){
+    // this.contador++;
+    const accion: Action = new IncrementarAction();
+    this.store.dispatch( accion );
+  }
+
+  decrementar(){
+    // this.contador--;
+    const accion: Action = new DecrementarAction();
+    this.store.dispatch( accion );
+  }
 }
